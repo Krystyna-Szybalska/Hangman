d8bef660e0f6fb3926afa17c413b4e7e79d69605
KRYSIAACER\Krysia <krystyna.szybalska@gmail.com>
2021-07-03 03:48:44 +0200

    Added HighScores file

--- Hangman/MainWindow.xaml.cs
+++ Hangman/MainWindow.xaml.cs
@@ -2,10 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -23,7 +24,7 @@ namespace Hangman
         private string currentCapital;
         public List<string> wrongGuesses = new();
         private readonly List<char> currentCapitalLetters = new();
-        private bool LetterGuessedCorrectly
+        public bool LetterGuessed
         {
             get
             {
@@ -31,7 +32,7 @@ namespace Hangman
                 else return false;
             }
         }
-        private bool WordGuessedCorrectly
+        public bool WordGuessed
         {
             get
             {
@@ -51,18 +52,38 @@ namespace Hangman
             }
             return true;
         }
+        private int _guessingTries = 0;
+        private static Stopwatch _stopwatch = new();
+        private long _guessingTime;
+        private string _guessedWord;
+        private string _highScoreString;
+        public string HighScoreString
+        {
+            get
+            {
+                return _highScoreString;
+            }
+            set
+            {
+                _highScoreString = " | " + DateTime.Today.ToString() + " | " + _guessingTime.ToString() + " | " + _guessingTries + " | " + _guessedWord;
+            }
+        } 
+
         public MainWindow()
         {
             InitializeComponent();
             this.DataContext = this;
             StartNewGame();
         }
-        private void StartNewGame()
+        public void StartNewGame()
         {
+            _guessingTries = 0;
             hintTextBlock.Visibility = Visibility.Collapsed;
+            wrongGuessesTextBlock.Visibility = Visibility.Collapsed;
             SelectNewCapitalToGuess();
             CreateGuessingArea();
             RefreshLifePoints();
+            _stopwatch.Restart();
         }
         private void SelectNewCapitalToGuess()
         {
@@ -70,7 +91,7 @@ namespace Hangman
             currentCapital = words.SelectRandomWord(words.GetListOfCapitals());
             hintTextBlock.Text = string.Format("Hint: It's the capital of {0}", words.GetListOfCountries()[words.ListIndex]);
             wrongGuesses.Clear();
-            wrongGuessesTextBlock.Text = "";
+            wrongGuessesList.Text = "";
             currentCapitalLetters.Clear();
             currentCapitalLetters.AddRange(currentCapital.ToUpper());
         }
@@ -89,7 +110,7 @@ namespace Hangman
                         Height = 150,
                         Text = " ",
                         FontSize = 75,
-                        TextAlignment = TextAlignment.Center,
+                        TextAlignment = TextAlignment.Justify,
                         VerticalAlignment = VerticalAlignment.Bottom
                     };
 
@@ -107,7 +128,7 @@ namespace Hangman
                         Height = 150,
                         FontSize = 75,
                         Text = "_",
-                        TextAlignment = TextAlignment.Center,
+                        TextAlignment = TextAlignment.Justify,
                         VerticalAlignment = VerticalAlignment.Bottom,
                 };
 
@@ -151,6 +172,7 @@ namespace Hangman
 
             else
             {
+                _stopwatch.Stop();
                 HP5.Visibility = Visibility.Collapsed;
                 AdonisUI.Controls.MessageBoxResult result = AdonisUI.Controls.MessageBox.Show("You've died! Do you want to start a new game?", "Game over!", AdonisUI.Controls.MessageBoxButton.YesNo);
                 if (result == AdonisUI.Controls.MessageBoxResult.Yes)
@@ -179,7 +201,7 @@ namespace Hangman
                     typeYourGuessTextbox.Text = null;
                 }
 
-                else if (LetterGuessedCorrectly)
+                else if (LetterGuessed)
                 {
                     List<string> guessedLetters = new();
 
@@ -202,37 +224,37 @@ namespace Hangman
                         TextBlock letterTextBlock = GuessingArea.Children[indexList[i]] as TextBlock;
                         letterTextBlock.Text = string.Format("{0}", typeYourGuessTextbox.Text.ToUpper());
                         guessedLetters.Add(letterTextBlock.Text.ToUpper());
+                        _guessingTries++;
                     }
+                    
                     typeYourGuessTextbox.Text = null;
 
                     if (TheWordIsGuessed())
                     {
-                        AdonisUI.Controls.MessageBoxResult result = AdonisUI.Controls.MessageBox.Show("You've won! Do you want to start a new game?", "Congratulations!", AdonisUI.Controls.MessageBoxButton.YesNo);
-                        if (result == AdonisUI.Controls.MessageBoxResult.Yes)
-                        {
-                            StartNewGame();
-                        }
-                        else if (result == AdonisUI.Controls.MessageBoxResult.No)
-                        {
-                            this.Close();
-                        }
+                        _stopwatch.Stop();
+                        _guessingTime = _stopwatch.ElapsedMilliseconds / 1000;
+                        _guessedWord = currentCapital;
+
+                        ShowWinningMessageBox();
                     }
                 }
 
                 else
                 {
                     wrongGuesses.Add(typeYourGuessTextbox.Text.ToUpper());
-                    wrongGuessesTextBlock.Text += typeYourGuessTextbox.Text.ToUpper() + ' ';
+                    wrongGuessesTextBlock.Visibility = Visibility.Visible;
+                    wrongGuessesList.Text += typeYourGuessTextbox.Text.ToUpper() + ' ';
                     typeYourGuessTextbox.Text = null;
+                    _guessingTries++;
                     LoseLifePoint();
                 }
             }
         }
         private void CheckTypedWord()
         {
-            if ((typeYourGuessTextbox.Text).Length != currentCapital.Length || !(typeYourGuessTextbox.Text).All(Char.IsLetter))
+            if ((typeYourGuessTextbox.Text).Length != currentCapital.Length)
             {
-                AdonisUI.Controls.MessageBox.Show(string.Format("Type in a {0}-letter word", currentCapital.Length), "Info", AdonisUI.Controls.MessageBoxButton.OK);
+                AdonisUI.Controls.MessageBox.Show(string.Format("Enter a {0}-letter word", currentCapital.Length), "Info", AdonisUI.Controls.MessageBoxButton.OK);
             }
 
             else
@@ -243,8 +265,13 @@ namespace Hangman
                     typeYourGuessTextbox.Text = null;
                 }
 
-                else if (WordGuessedCorrectly)
+                else if (WordGuessed)
                 {
+                    _guessingTries++;
+                    _stopwatch.Stop();
+                    _guessingTime = _stopwatch.ElapsedMilliseconds / 1000;
+                    _guessedWord = currentCapital;
+
                     for (int i = 0; i < GuessingArea.Children.Count; i++)
                     {
                         TextBlock letterTextBlock = GuessingArea.Children[i] as TextBlock;
@@ -252,22 +279,16 @@ namespace Hangman
                     }
                     typeYourGuessTextbox.Text = null;
 
-                    AdonisUI.Controls.MessageBoxResult result = AdonisUI.Controls.MessageBox.Show("You've won! Do you want to start a new game?", "Congratulations!", AdonisUI.Controls.MessageBoxButton.YesNo);
-                    if (result == AdonisUI.Controls.MessageBoxResult.Yes)
-                    {
-                        StartNewGame();
-                    }
-                    else if (result == AdonisUI.Controls.MessageBoxResult.No)
-                    {
-                        this.Close();
-                    }
+                    ShowWinningMessageBox();
                 }
 
                 else
                 {
                     typeYourGuessTextbox.Text = null;
                     wrongGuesses.Add(typeYourGuessTextbox.Text.ToUpper());
-                    wrongGuessesTextBlock.Text += typeYourGuessTextbox.Text.ToUpper() + ' ';
+                    wrongGuessesTextBlock.Visibility = Visibility.Visible;
+                    wrongGuessesList.Text += typeYourGuessTextbox.Text.ToUpper() + ' ';
+                    _guessingTries++;
                     LoseLifePoint();
                     LoseLifePoint();
                 }
@@ -280,6 +301,41 @@ namespace Hangman
         private void GuessWordButton_Click(object sender, RoutedEventArgs e)
         {
             CheckTypedWord();
+        }
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (typeYourGuessTextbox.Text.Length == 1) CheckTypedLetter();
+                else if (typeYourGuessTextbox.Text.Length == currentCapital.Length) CheckTypedWord();
+                else
+                {
+                    AdonisUI.Controls.MessageBox.Show(string.Format("Enter one letter of the English alphabet or a {0}-letter word", currentCapital.Length), "Info", AdonisUI.Controls.MessageBoxButton.OK);
+                }
+            }
+        }
+        private void ShowWinningMessageBox()
+        {
+
+            AdonisUI.Controls.MessageBoxResult result1 = AdonisUI.Controls.MessageBox.Show("You've won! Do you want to save your score?", "Congratulations!", AdonisUI.Controls.MessageBoxButton.YesNo);
+            if (result1 == AdonisUI.Controls.MessageBoxResult.Yes)
+            {
+                EnterYourNameWIndow window = new();
+                window.Show();
+                StartNewGame();
+            }
+            else if (result1 == AdonisUI.Controls.MessageBoxResult.No)
+            {
+                AdonisUI.Controls.MessageBoxResult result2 = AdonisUI.Controls.MessageBox.Show("Do you want to start a new game?", "Congratulations!", AdonisUI.Controls.MessageBoxButton.YesNo);
+                if (result2 == AdonisUI.Controls.MessageBoxResult.Yes)
+                {
+                    StartNewGame();
+                }
+                else if (result2 == AdonisUI.Controls.MessageBoxResult.No)
+                {
+                    this.Close();
+                }
+            }
         }
     }
 }
